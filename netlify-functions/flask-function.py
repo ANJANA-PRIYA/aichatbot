962633fce56290d2c699263e794d43f3a3a17b0d
ANJANA-PRIYA <anjanapriyaguntiapg@gmail.com>
2025-05-08 21:29:17 +0530

    Updated app.py, HTML, and server.js for deployment

--- netlify-functions/flask-function.py
+++ netlify-functions/flask-function.py
@@ -1,3 +1,18 @@
+
+from flask import Flask
+from serverless import handler
+
+# Create the Flask app
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, world!"
+
+# Adapt the Flask app to be a serverless function
+def lambda_handler(event, context):
+    return handler(app, event, context)
+
 from flask import Flask
 from serverless import handler
 
