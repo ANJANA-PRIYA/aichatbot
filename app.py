f825a612112d430b363d005a737437d78f183827
ANJANA-PRIYA <anjanapriyaguntiapg@gmail.com>
2025-05-09 21:17:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')  # This should be in the 'templates' folder
+    return render_template('index.html')
 
 @app.route('/api/get', methods=['POST'])
 def get_response():
-    user_msg = request.form.get('msg', '')
-    return jsonify({'response': f'You said: {user_msg}'})  # Use 'response' as the key
+    data = request.form
+    user_msg = data.get('msg', '')
+    return jsonify({'text': f'You said: {user_msg}'})
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
+
